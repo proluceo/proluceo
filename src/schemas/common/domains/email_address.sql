2d7067793b1d8628b13c98abe72b3aea5032f9f8
Bruno Enten <bruno@enten.me>
2023-07-25 16:26:41 +0300

    move email domain and rename it

--- src/schemas/common/domains/email_address.sql
+++ src/schemas/common/domains/email_address.sql
@@ -1,3 +1,3 @@
 -- depends_on: ["::schemas:public:extensions:citext"]
-CREATE DOMAIN public.email AS citext
+CREATE DOMAIN common.email_address AS citext
   CHECK ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
